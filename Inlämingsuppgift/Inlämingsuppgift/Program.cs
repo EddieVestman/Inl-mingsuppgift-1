7d38e61924a0eab4fb94520503bd10a0b1f73ec6
EddieVestman <edve02001@utb.vaxjo.se>
2019-09-06 09:45:27 +0200

    Skrev in en del kod om RNG

---
+++ Inlämingsuppgift/Inlämingsuppgift/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Inlämingsuppgift
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random generator = new Random();
+            int rand = generator.Next(1, 100);
+            Console.WriteLine(rand);
+            Console.WriteLine("Gissa ett tal mellan 1-100: ");
+             int svar = Console.ReadLine();
+            if (svar == rand )
+            
+        }
+    }
+}
