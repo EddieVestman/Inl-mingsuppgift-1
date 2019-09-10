dcd2278194b45aeb5083dc694e4dd0eddcebfbaa
EddieVestman <edve02001@utb.vaxjo.se>
2019-09-10 12:53:37 +0200

    Gjorde klart koden / gjorde loopen

--- Inlämingsuppgift/Inlämingsuppgift/Program.cs
+++ Inlämingsuppgift/Inlämingsuppgift/Program.cs
@@ -7,12 +7,28 @@ namespace Inlämingsuppgift
         static void Main()
         {
             Random generator = new Random();
-            int rand = generator.Next(1, 100);
-            Console.WriteLine(rand);
+            int rand = generator.Next(1, 100), gissningar = 0;
             Console.WriteLine("Gissa ett tal mellan 1-100: ");
-             int svar = Console.ReadLine();
-            if (svar == rand )
+            int tal = Convert.ToInt32(Console.ReadLine());
+            while(tal != rand)
+            {
+                if (tal < rand)
+                {
+                    Console.WriteLine("Du gissade för lågt!");
+
+                }
+                else if (tal > rand)
+                {
+                    Console.WriteLine("Du gissade för högt!");
+                }
+                tal = Convert.ToInt32(Console.ReadLine());
+                gissningar++;
+            }
+            Console.WriteLine("Du gissade rätt!");
+            Console.WriteLine("Du gissade " + gissningar + " gånger");
             
+
+
         }
     }
 }
